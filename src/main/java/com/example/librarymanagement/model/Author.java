afb9f9a3d66898324fa1b8e80557543e6e2d183d
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-07-21 00:20:46 +0400

    Author və Book klaslarını kodlaşdırma

--- src/main/java/com/example/librarymanagement/model/Author.java
+++ src/main/java/com/example/librarymanagement/model/Author.java
@@ -1,4 +1,31 @@
 package com.example.librarymanagement.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String biography;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
 }
--- src/main/java/com/example/librarymanagement/model/Book.java
+++ src/main/java/com/example/librarymanagement/model/Book.java
@@ -1,4 +1,30 @@
 package com.example.librarymanagement.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(unique = true, nullable = false, length = 13)
+    private String isbn;
+
+    private String genre;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
 }
