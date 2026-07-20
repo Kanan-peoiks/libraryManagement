ebe688f68242563dac7244cfe75610e5d2d55d08
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-07-21 00:53:23 +0400

    annotasiyanın əlavə edilməsi

--- src/main/java/com/example/librarymanagement/repository/AuthorRepo.java
+++ src/main/java/com/example/librarymanagement/repository/AuthorRepo.java
@@ -2,7 +2,8 @@ package com.example.librarymanagement.repository;
 
 import com.example.librarymanagement.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuthorRepo extends JpaRepository<Author,Long>
-{
+@Repository
+public interface AuthorRepo extends JpaRepository<Author,Long>{
 }
