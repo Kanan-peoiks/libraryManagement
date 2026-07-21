4667d2f4ac84643f99722c203b1277009ccdf759
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-07-21 10:47:55 +0400

    Member-in RequestDto-sunun yaradılması

--- src/main/java/com/example/librarymanagement/dto/MemberRequestDTO.java
+++ src/main/java/com/example/librarymanagement/dto/MemberRequestDTO.java
@@ -1,4 +1,14 @@
 package com.example.librarymanagement.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class MemberRequestDTO {
+    @NotBlank(message = "İstifadəçinin adı boş qala bilməz.")
+    private String name;
+    @NotBlank(message = "Email boş qala bilməz.")
+    @Email(message = "Email düzgün daxil edilməyib.")
+    private String email;
 }
--- src/main/java/com/example/librarymanagement/dto/MemberResponseDTO.java
+++ src/main/java/com/example/librarymanagement/dto/MemberResponseDTO.java
@@ -1,4 +1,9 @@
 package com.example.librarymanagement.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class MemberResponseDTO {
+
 }
