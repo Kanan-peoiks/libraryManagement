939ee785f1a752ad7eb6994fad36a324e7147ec1
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-07-21 17:42:03 +0400

    MemberController üçün kontrollerin yazılması

---
+++ src/main/java/com/example/librarymanagement/controller/MemberController.java
@@ -0,0 +1,49 @@
+package com.example.librarymanagement.controller;
+
+import com.example.librarymanagement.dto.MemberRequestDTO;
+import com.example.librarymanagement.dto.MemberResponseDTO;
+import com.example.librarymanagement.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<MemberResponseDTO> createMember(@Valid @RequestBody MemberRequestDTO requestDTO) {
+        MemberResponseDTO createdMember = memberService.createMember(requestDTO);
+        return new ResponseEntity<>(createdMember, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<MemberResponseDTO>> getAllMembers(Pageable pageable) {
+        return ResponseEntity.ok(memberService.getAllMembers(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberResponseDTO> getMemberById(@PathVariable Long id) {
+        return ResponseEntity.ok(memberService.getMemberById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MemberResponseDTO> updateMember(
+            @PathVariable Long id,
+            @Valid @RequestBody MemberRequestDTO requestDTO) {
+        return ResponseEntity.ok(memberService.updateMember(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMember(@PathVariable Long id) {
+        memberService.deleteMember(id);
+        return ResponseEntity.noContent().build();
+    }
+}
