5a7b7105de3ed18e53594f675d4f72d259abe59c
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-07-21 17:25:14 +0400

    AuthorController üçün kontrollerin yazılması

---
+++ src/main/java/com/example/librarymanagement/controller/AuthorController.java
@@ -0,0 +1,49 @@
+package com.example.librarymanagement.controller;
+
+import com.example.librarymanagement.dto.AuthorRequestDTO;
+import com.example.librarymanagement.dto.AuthorResponseDTO;
+import com.example.librarymanagement.service.AuthorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/authors")
+@RequiredArgsConstructor
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @PostMapping
+    public ResponseEntity<AuthorResponseDTO> createAuthor(@Valid @RequestBody AuthorRequestDTO requestDTO) {
+        AuthorResponseDTO createdAuthor = authorService.createAuthor(requestDTO);
+        return new ResponseEntity<>(createdAuthor, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AuthorResponseDTO>> getAllAuthors(Pageable pageable) {
+        return ResponseEntity.ok(authorService.getAllAuthors(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorResponseDTO> getAuthorById(@PathVariable Long id) {
+        return ResponseEntity.ok(authorService.getAuthorById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AuthorResponseDTO> updateAuthor(
+            @PathVariable Long id,
+            @Valid @RequestBody AuthorRequestDTO requestDTO) {
+        return ResponseEntity.ok(authorService.updateAuthor(id, requestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+        authorService.deleteAuthor(id);
+        return ResponseEntity.noContent().build();
+    }
+}
