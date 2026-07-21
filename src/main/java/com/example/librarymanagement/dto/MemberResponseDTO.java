0c011db805a5367b105081faab6f66d392f5efa9
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-07-21 10:48:43 +0400

    Member-in ResponseDTO-sunun yaradılması

--- src/main/java/com/example/librarymanagement/dto/MemberResponseDTO.java
+++ src/main/java/com/example/librarymanagement/dto/MemberResponseDTO.java
@@ -3,7 +3,12 @@ package com.example.librarymanagement.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class MemberResponseDTO {
-
+    private String memberId;
+    private String name;
+    private String email;
+    private LocalDate memberBirthDate;
 }
