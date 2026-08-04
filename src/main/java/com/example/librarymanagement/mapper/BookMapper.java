f0279462a0a542388c1cf8c55570085b59fe7a6b
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-05 00:05:03 +0400

    book üçün mapper yazıldı

---
+++ src/main/java/com/example/librarymanagement/mapper/BookMapper.java
@@ -0,0 +1,33 @@
+package com.example.librarymanagement.mapper;
+
+import com.example.librarymanagement.dto.BookRequestDTO;
+import com.example.librarymanagement.dto.BookResponseDTO;
+import com.example.librarymanagement.model.Author;
+import com.example.librarymanagement.model.Book;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookMapper {
+
+    public Book toEntity(BookRequestDTO dto, Author author) {
+        Book book = new Book();
+        book.setTitle(dto.getTitle());
+        book.setIsbn(dto.getIsbn());
+        book.setGenre(dto.getGenre());
+        book.setAuthor(author);
+        return book;
+    }
+
+    public BookResponseDTO toResponseDTO(Book book) {
+        BookResponseDTO responseDTO = new BookResponseDTO();
+        responseDTO.setId(book.getId());
+        responseDTO.setTitle(book.getTitle());
+        responseDTO.setIsbn(book.getIsbn());
+        responseDTO.setGenre(book.getGenre());
+        if (book.getAuthor() != null) {
+            responseDTO.setAuthorId(book.getAuthor().getId());
+            responseDTO.setAuthorName(book.getAuthor().getName());
+        }
+        return responseDTO;
+    }
+}
--- src/main/java/com/example/librarymanagement/mapper/BookMember.java
+++
@@ -1,4 +0,0 @@
-package com.example.librarymanagement.mapper;
-
-public class BookMember {
-}
