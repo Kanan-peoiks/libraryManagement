e851ecfa84d05f2924d5887319fdd06c8ef099c9
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-05 00:22:59 +0400

    mapper paketinin bitirilməsi

--- src/main/java/com/example/librarymanagement/mapper/AuthorMapper.java
+++ src/main/java/com/example/librarymanagement/mapper/AuthorMapper.java
@@ -1,4 +1,25 @@
 package com.example.librarymanagement.mapper;
 
+import com.example.librarymanagement.dto.AuthorRequestDTO;
+import com.example.librarymanagement.dto.AuthorResponseDTO;
+import com.example.librarymanagement.model.Author;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AuthorMapper {
+
+    public Author toEntity(AuthorRequestDTO dto) {
+        Author author = new Author();
+        author.setName(dto.getName());
+        author.setBiography(dto.getBiography());
+        return author;
+    }
+
+    public AuthorResponseDTO toResponseDTO(Author author) {
+        AuthorResponseDTO responseDTO = new AuthorResponseDTO();
+        responseDTO.setId(author.getId());
+        responseDTO.setName(author.getName());
+        responseDTO.setBiography(author.getBiography());
+        return responseDTO;
+    }
 }
--- src/main/java/com/example/librarymanagement/mapper/MemberMapper.java
+++ src/main/java/com/example/librarymanagement/mapper/MemberMapper.java
@@ -1,4 +1,27 @@
 package com.example.librarymanagement.mapper;
 
+
+import com.example.librarymanagement.dto.MemberRequestDTO;
+import com.example.librarymanagement.dto.MemberResponseDTO;
+import com.example.librarymanagement.model.Member;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberMapper {
+
+    public Member toEntity(MemberRequestDTO dto) {
+        Member member = new Member();
+        member.setName(dto.getName());
+        member.setEmail(dto.getEmail());
+        return member;
+    }
+
+    public MemberResponseDTO toResponseDTO(Member member) {
+        MemberResponseDTO responseDTO = new MemberResponseDTO();
+        responseDTO.setMemberId(member.getId());
+        responseDTO.setName(member.getName());
+        responseDTO.setEmail(member.getEmail());
+        responseDTO.setMemberBirthDate(member.getMembershipDate());
+        return responseDTO;
+    }
 }
