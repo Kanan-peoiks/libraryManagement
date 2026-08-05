b9bc2223fc55bec038b889862e55941aa9c0346f
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-05 16:43:38 +0400

    member controllerin yenilənməsi

--- src/main/java/com/example/librarymanagement/controller/MemberController.java
+++ src/main/java/com/example/librarymanagement/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.example.librarymanagement.controller;
 import com.example.librarymanagement.dto.MemberRequestDTO;
 import com.example.librarymanagement.dto.MemberResponseDTO;
 import com.example.librarymanagement.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,38 +13,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
+@Tag(name = "Member Controller", description = "Üzvlərin idarə olunması üçün API-lər")
 public class MemberController {
 
     private final MemberService memberService;
 
     @PostMapping
+    @Operation(summary = "Yeni üzv qeydiyyatı")
     public ResponseEntity<MemberResponseDTO> createMember(@Valid @RequestBody MemberRequestDTO requestDTO) {
-        MemberResponseDTO createdMember = memberService.createMember(requestDTO);
-        return new ResponseEntity<>(createdMember, HttpStatus.CREATED);
+        return new ResponseEntity<>(memberService.createMember(requestDTO), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Bütün üzvləri səhifələmə (pagination) ilə gətirmək")
     public ResponseEntity<Page<MemberResponseDTO>> getAllMembers(Pageable pageable) {
         return ResponseEntity.ok(memberService.getAllMembers(pageable));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "ID-yə görə üzvü gətirmək")
     public ResponseEntity<MemberResponseDTO> getMemberById(@PathVariable Long id) {
         return ResponseEntity.ok(memberService.getMemberById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<MemberResponseDTO> updateMember(
-            @PathVariable Long id,
-            @Valid @RequestBody MemberRequestDTO requestDTO) {
+    @Operation(summary = "ID-yə görə üzv məlumatlarını yeniləmək")
+    public ResponseEntity<MemberResponseDTO> updateMember(@PathVariable Long id, @Valid @RequestBody MemberRequestDTO requestDTO) {
         return ResponseEntity.ok(memberService.updateMember(id, requestDTO));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "ID-yə görə üzvü silmək")
     public ResponseEntity<Void> deleteMember(@PathVariable Long id) {
         memberService.deleteMember(id);
         return ResponseEntity.noContent().build();
