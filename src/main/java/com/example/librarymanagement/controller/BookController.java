4ce189e71ce2a6a283d691b19fb844375889759b
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-05 16:43:14 +0400

    book controllerin yenilənməsi

--- src/main/java/com/example/librarymanagement/controller/BookController.java
+++ src/main/java/com/example/librarymanagement/controller/BookController.java
@@ -3,6 +3,8 @@ package com.example.librarymanagement.controller;
 import com.example.librarymanagement.dto.BookRequestDTO;
 import com.example.librarymanagement.dto.BookResponseDTO;
 import com.example.librarymanagement.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,38 +13,40 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
+@Tag(name = "Book Controller", description = "Kitabların idarə olunması üçün API-lər")
 public class BookController {
 
     private final BookService bookService;
 
     @PostMapping
+    @Operation(summary = "Yeni kitab əlavə etmək")
     public ResponseEntity<BookResponseDTO> createBook(@Valid @RequestBody BookRequestDTO requestDTO) {
-        BookResponseDTO createdBook = bookService.createBook(requestDTO);
-        return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+        return new ResponseEntity<>(bookService.createBook(requestDTO), HttpStatus.CREATED);
     }
 
     @GetMapping
+    @Operation(summary = "Bütün kitabları səhifələmə (pagination) ilə gətirmək")
     public ResponseEntity<Page<BookResponseDTO>> getAllBooks(Pageable pageable) {
         return ResponseEntity.ok(bookService.getAllBooks(pageable));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "ID-yə görə kitabı gətirmək")
     public ResponseEntity<BookResponseDTO> getBookById(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getBookById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<BookResponseDTO> updateBook(
-            @PathVariable Long id,
-            @Valid @RequestBody BookRequestDTO requestDTO) {
+    @Operation(summary = "ID-yə görə kitab məlumatlarını yeniləmək")
+    public ResponseEntity<BookResponseDTO> updateBook(@PathVariable Long id, @Valid @RequestBody BookRequestDTO requestDTO) {
         return ResponseEntity.ok(bookService.updateBook(id, requestDTO));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "ID-yə görə kitabı silmək")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
         return ResponseEntity.noContent().build();
