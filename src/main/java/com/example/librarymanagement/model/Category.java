de961238b65561639542313031f72144b187235b
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-12 15:21:06 +0400

    kateqoriya klasının əlavə edilməsi

---
+++ src/main/java/com/example/librarymanagement/model/Category.java
@@ -0,0 +1,27 @@
+package com.example.librarymanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories", fetch = FetchType.LAZY)
+    @Builder.Default
+    private Set<Book> books = new HashSet<>();
+}
