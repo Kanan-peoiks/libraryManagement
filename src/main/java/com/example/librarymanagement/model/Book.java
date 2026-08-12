310afad62e13b4d257f4298f60156931e35691a2
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-12 15:21:43 +0400

    book sinifinin yenilənməsi

--- src/main/java/com/example/librarymanagement/model/Book.java
+++ src/main/java/com/example/librarymanagement/model/Book.java
@@ -3,6 +3,9 @@ package com.example.librarymanagement.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "books")
 @Getter
@@ -19,12 +22,26 @@ public class Book {
     @Column(nullable = false)
     private String title;
 
-    @Column(unique = true, nullable = false, length = 13)
+    @Column(nullable = false, unique = true)
     private String isbn;
 
     private String genre;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id", nullable = false)
+    @JoinColumn(name = "author_id")
     private Author author;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "book_categories",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    @Builder.Default
+    private Set<Category> categories = new HashSet<>();
+
+    public void addCategory(Category category) {
+        this.categories.add(category);
+        category.getBooks().add(this);
+    }
 }
