f9be22668577fe92cd2777981a871948d3b2e9f1
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-13 00:12:23 +0400

    Book üçün sorğular yazılması: janra görə seçmə, kateqoriyaya görə seçmə və ən çox oxunan 5 kitabı seçmə

--- src/main/java/com/example/librarymanagement/repository/BookRepe.java
+++ src/main/java/com/example/librarymanagement/repository/BookRepe.java
@@ -2,8 +2,33 @@ package com.example.librarymanagement.repository;
 
 import com.example.librarymanagement.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepe extends JpaRepository<Book, Long> {
-}
+
+        Optional<Book> findByIsbn(String isbn);
+
+        //janra görə çağırma
+        List<Book> findByTitleContainingIgnoreCaseAndGenreOrderByTitleAsc(String title, String genre);
+
+    //kateqoriyaya görə çağırma
+        @Query("SELECT b FROM Book b JOIN b.categories c WHERE LOWER(c.name) = LOWER(:categoryName)")
+        List<Book> findBooksByCategoryName(@Param("categoryName") String categoryName);
+
+
+//top 5 kitablar
+        @Query(value = """
+            SELECT b.* FROM books b
+            JOIN borrow_items bi ON b.id = bi.book_id
+            GROUP BY b.id
+            ORDER BY COUNT(bi.id) DESC
+            LIMIT :limit
+            """, nativeQuery = true)
+        List<Book> findTopBorrowedBooks(@Param("limit") int limit);
+    }
