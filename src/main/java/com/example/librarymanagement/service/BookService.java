3cdc28ee86c9524fcd07154963e086e54f9a3bd4
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-16 12:57:14 +0400

    Repo üçün yeni interfeys və Service qatı üçün axtarışın əlavə edilməsi

--- src/main/java/com/example/librarymanagement/service/BookService.java
+++ src/main/java/com/example/librarymanagement/service/BookService.java
@@ -2,15 +2,18 @@ package com.example.librarymanagement.service;
 
 import com.example.librarymanagement.dto.BookRequestDTO;
 import com.example.librarymanagement.dto.BookResponseDTO;
+import com.example.librarymanagement.dto.BookSearch;
 import com.example.librarymanagement.exception.NotFoundException;
 import com.example.librarymanagement.mapper.BookMapper;
 import com.example.librarymanagement.model.Author;
 import com.example.librarymanagement.model.Book;
 import com.example.librarymanagement.repository.AuthorRepo;
 import com.example.librarymanagement.repository.BookRepe;
+import com.example.librarymanagement.specification.BookSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -65,5 +68,12 @@ public class BookService {
             throw new NotFoundException("Silinəcək kitab tapılmadı. ID: " + id);
         }
         bookRepository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<BookResponseDTO> searchBooks(BookSearch search, Pageable pageable) {
+        Specification<Book> spec = BookSpecification.filterBooks(search);
+        return bookRepository.findAll(spec, pageable)
+                .map(bookMapper::toResponseDTO);
     }
 }
