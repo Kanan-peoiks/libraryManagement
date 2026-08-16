c0b640df1995b9e1029e9abfc3144000877a1231
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-16 14:01:24 +0400

    checkpoint 4 üçün dto paketlərin yaradılması

--- src/main/java/com/example/librarymanagement/dto/CreateBorrowOrderRequestDTO.java
+++ src/main/java/com/example/librarymanagement/dto/CreateBorrowOrderRequestDTO.java
@@ -1,11 +1,23 @@
 package com.example.librarymanagement.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Data
 @Getter
 @Setter
 public class CreateBorrowOrderRequestDTO {
+
+    @NotNull(message = "İstifadəçi ID-si mütləq qeyd olunmalıdır.")
+    private Long userId;
+
+    @NotEmpty(message = "Sifarişdə ən azı 1 kitab olmalıdır")
+    @Valid
+    private List<BorrowItemRequestDTO> items;
 }
