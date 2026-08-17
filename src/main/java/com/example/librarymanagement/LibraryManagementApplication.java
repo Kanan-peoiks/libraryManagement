94123762ef8559320f262a144265865180f4c47a
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-08-17 22:56:16 +0400

    Əsas sinifimizə keş məntiqinin (konfiqurasiya) əlavə edilməsi

--- src/main/java/com/example/librarymanagement/LibraryManagementApplication.java
+++ src/main/java/com/example/librarymanagement/LibraryManagementApplication.java
@@ -2,12 +2,13 @@ package com.example.librarymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LibraryManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagementApplication.class, args);
     }
-
 }
